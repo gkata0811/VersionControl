370bea07bccee33ea63b8df3be0f1ba4e942edab
gulya <gulya@GKASUSNB>
2023-03-18 15:08:43 +0100

    excel letrehozasa

--- gsqrbp_irfgyak4/gsqrbp_irfgyak4/Form1.cs
+++ gsqrbp_irfgyak4/gsqrbp_irfgyak4/Form1.cs
@@ -16,6 +16,10 @@ namespace gsqrbp_irfgyak4
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         
         public Form1()
         {
