14246baab8c1862f3a459d69c792220f03005a07
Kata Gulyás <kata.gulyas@stud.uni-corvinus.hu>
2023-03-28 08:07:57 +0200

    XmlDocument

--- gsqrbp_irfgyak5/gsqrbp_irfgyak5/Form1.cs
+++ gsqrbp_irfgyak5/gsqrbp_irfgyak5/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace gsqrbp_irfgyak5
 {
@@ -34,6 +35,21 @@ namespace gsqrbp_irfgyak5
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new Entities.RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
