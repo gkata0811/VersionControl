1f126bfd4b4b9edb81614d77f10f234ccc881d5e
gulya <gulya@GKASUSNB>
2023-04-02 16:40:21 +0200

    Balls in panel

--- gsqrbp_irfgyak6/gsqrbp_irfgyak6/Form1.cs
+++ gsqrbp_irfgyak6/gsqrbp_irfgyak6/Form1.cs
@@ -12,9 +12,44 @@ namespace gsqrbp_irfgyak6
 {
     public partial class Form1 : Form
     {
+        private List<Entities.Ball> _balls = new List<Entities.Ball>();
+        private Entities.BallFactory _factory;
+        public Entities.BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+        
         public Form1()
         {
             InitializeComponent();
+
+            Factory = new Entities.BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
         }
     }
 }
