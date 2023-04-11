4b9819b61f250e4491ff5aea23e3818f86843417
Kata Gulyás <kata.gulyas@stud.uni-corvinus.hu>
2023-04-11 08:31:26 +0200

    GetPortfolioValue and DateTime

--- gsqrbp_irfgyak7/gsqrbp_irfgyak7/Form1.cs
+++ gsqrbp_irfgyak7/gsqrbp_irfgyak7/Form1.cs
@@ -23,6 +23,23 @@ namespace gsqrbp_irfgyak7
             Ticks = context.Ticks.ToList();
             dgvTicks.DataSource = Ticks;
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervallum = 30;
+            DateTime kezdoDatum = (from x in Ticks select x.TradingDay).Min();
+            DateTime zaroDatum = new DateTime(2016, 12, 30);
+            TimeSpan z = zaroDatum - kezdoDatum;
+            for (int i = 0; i < z.Days - intervallum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdoDatum.AddDays(i + intervallum))
+                    - GetPortfolioValue(kezdoDatum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+            var nyeresegekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x).ToList();
+            MessageBox.Show(nyeresegekRendezve[nyeresegekRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
@@ -31,6 +48,20 @@ namespace gsqrbp_irfgyak7
             Portfolio.Add(new Entities.PortfolioItem() { Index = "ZWACK", Volume = 10 });
             Portfolio.Add(new Entities.PortfolioItem() { Index = "ELMU", Volume = 10 });
             dgvPortfolio.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                            && date <= x.TradingDay
+                            select x).First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
