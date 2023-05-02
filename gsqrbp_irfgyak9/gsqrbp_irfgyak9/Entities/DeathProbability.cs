741a4d844d75e2d12b14a25a79532c391196eab8
gulya <gulya@GKASUSNB>
2023-05-02 20:01:40 +0200

    classes

---
+++ gsqrbp_irfgyak9/gsqrbp_irfgyak9/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gsqrbp_irfgyak9.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double deathProbability { get; set; }
+    }
+}
