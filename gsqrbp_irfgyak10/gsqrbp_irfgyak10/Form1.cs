8cb8c748f3e7dbd448e06f15a777204125069396
gulya <gulya@GKASUSNB>
2023-05-09 08:46:59 +0200

    Winner

--- gsqrbp_irfgyak10/gsqrbp_irfgyak10/Form1.cs
+++ gsqrbp_irfgyak10/gsqrbp_irfgyak10/Form1.cs
@@ -21,6 +21,8 @@ namespace gsqrbp_irfgyak10
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+        Brain winnerBrain = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -48,6 +50,16 @@ namespace gsqrbp_irfgyak10
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
