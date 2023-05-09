21f4d6090d8d266cc6c4560a389694f50c49ce4b
gulya <gulya@GKASUSNB>
2023-05-09 08:42:56 +0200

    Population

--- gsqrbp_irfgyak10/gsqrbp_irfgyak10/Form1.cs
+++ gsqrbp_irfgyak10/gsqrbp_irfgyak10/Form1.cs
@@ -16,6 +16,11 @@ namespace gsqrbp_irfgyak10
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
